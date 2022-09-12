6143b572894f0f675fea3013216188b6a3fba206
VladiMIR-y550 <mironenko.vladimir2019@gmail.com>
2022-09-12 14:53:09 +0300

    refactored ping pong ChatSocketClient

--- app/src/main/java/com/mironenko/internship_socket_chat/data/socket/ChatSocketClient.kt
+++ app/src/main/java/com/mironenko/internship_socket_chat/data/socket/ChatSocketClient.kt
@@ -2,23 +2,23 @@ package com.mironenko.internship_socket_chat.data.socket
 
 import com.google.gson.Gson
 import com.mironenko.internship_socket_chat.data.socket.model.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import java.io.*
 import java.net.*
 import javax.inject.Inject
 
 const val SOCKET_TIMEOUT = 10000
+const val PING_TIMEOUT = 8000L
 
 class ChatSocketClient @Inject constructor(
 ) : ChatSocket {
     private val gsonObj = Gson()
-    private val clientScope = CoroutineScope(Dispatchers.IO)
+    private val job = SupervisorJob()
+    private val clientScope = CoroutineScope(Dispatchers.Default + job)
+    private var disconnectJob: Job? = null
     private var socketTCP: Socket? = null
     private var writer: PrintWriter? = null
     private var reader: BufferedReader? = null
@@ -29,7 +29,6 @@ class ChatSocketClient @Inject constructor(
 
     private var serverIp: String = ""
     private var userId: String = ""
-
 
     override suspend fun connectToServerUdp() {
         var isSocketAddress = false
@@ -73,34 +72,14 @@ class ChatSocketClient @Inject constructor(
         clientScope.launch {
             while (socketTCP?.isConnected == true) {
                 try {
-                    val baseDto = jsonToBaseDto(reader?.readLine().toString())
-
+                    val response = reader?.readLine() ?: continue
+                    val baseDto = gsonObj.fromJson(response, BaseDto::class.java)
                     when (baseDto.action) {
                         BaseDto.Action.CONNECTED -> {
-                            val connectedDto = getConnectedDto(baseDto)
-                            userId = connectedDto.id
-                            sendJson(
-                                jsonBaseDto(
-                                    baseDtoAction = BaseDto.Action.CONNECT,
-                                    id = userId,
-                                    login = login
-                                )
-                            )
-                            sendJson(
-                                jsonBaseDto(
-                                    baseDtoAction = BaseDto.Action.PING,
-                                    id = userId
-                                )
-                            )
+                            pingPongCycle(baseDto, login)
                         }
                         BaseDto.Action.PONG -> {
-                            val pongId = getPongDto(baseDto).id
-                            sendJson(
-                                jsonBaseDto(
-                                    baseDtoAction = BaseDto.Action.PING,
-                                    id = pongId
-                                )
-                            )
+                            cancelDisconnectJob()
                         }
                         else -> {
                             throw IllegalArgumentException("Wrong BaseDto.Action")
@@ -114,12 +93,55 @@ class ChatSocketClient @Inject constructor(
         return userId
     }
 
+    private fun sendPing(): Job {
+        return clientScope.launch {
+            delay(10000)
+            _isAuthorized.value = false
+            clientScope.coroutineContext.cancelChildren()
+            socketTCP?.close()
+            socketTCP = null
+        }
+    }
+
+    private fun pingPongCycle(baseDto: BaseDto, login: String): Job {
+        val connectedDto = gsonObj.fromJson(baseDto.payload, ConnectedDto::class.java)
+        userId = connectedDto.id
+        sendJson(
+            jsonBaseDto(
+                baseDtoAction = BaseDto.Action.CONNECT,
+                id = userId,
+                login = login
+            )
+        )
+        return clientScope.launch {
+            while (socketTCP?.isConnected == true) {
+                delay(PING_TIMEOUT)
+                sendJson(
+                    jsonBaseDto(
+                        baseDtoAction = BaseDto.Action.PING,
+                        id = userId
+                    )
+                )
+                disconnectJob = sendPing()
+            }
+        }
+    }
+
+    private fun cancelDisconnectJob() {
+        disconnectJob?.cancel()
+        disconnectJob = null
+    }
+
     private fun sendJson(baseDtoJson: String) {
         writer?.println(baseDtoJson)
         writer?.flush()
     }
 
-    private fun jsonBaseDto(baseDtoAction: BaseDto.Action, id: String, login: String = ""): String {
+    private fun jsonBaseDto(
+        baseDtoAction: BaseDto.Action,
+        id: String,
+        login: String = ""
+    ): String {
         return when (baseDtoAction) {
             BaseDto.Action.CONNECT -> {
                 val payload = gsonObj.toJson(ConnectDto(id = id, name = login))
@@ -135,18 +157,6 @@ class ChatSocketClient @Inject constructor(
                 throw IllegalArgumentException("Wrong BaseDto.Action")
             }
         }
-    }
-
-    private fun jsonToBaseDto(responseJson: String): BaseDto {
-        return gsonObj.fromJson(responseJson, BaseDto::class.java)
-    }
-
-    private fun getPongDto(baseDto: BaseDto): PongDto {
-        return gsonObj.fromJson(baseDto.payload, PongDto::class.java)
-    }
-
-    private fun getConnectedDto(baseDto: BaseDto): ConnectedDto {
-        return gsonObj.fromJson(baseDto.payload, ConnectedDto::class.java)
     }
 
     private fun splitSocketAddress(socketAddress: String): String {
